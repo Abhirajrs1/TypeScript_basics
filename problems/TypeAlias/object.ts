6784aa532e0c5bb8044f49a061756632dc7fd21b
Abhirajrs1 <132868953+Abhirajrs1@users.noreply.github.com>
2025-02-13 00:15:45 +0530

    Read only and combine types also added

--- problems/TypeAlias/object.ts
+++ problems/TypeAlias/object.ts
@@ -12,3 +12,40 @@ let my:Person={
 }
 
 console.log(my)
+
+// Use the readonly(can't change) and ?(To avoid the error while didn't assign values)
+
+type Per={
+    readonly name:string,
+    age:number,
+    place:string,
+    creditCard?:number
+}
+let myn:Per={
+    name:"Abhiraj",
+    age:25,
+    place:"Thiruvananthapuram"
+}
+
+console.log(myn)
+
+// Use & to combine 2 or more type and assign values
+
+type cardNumber={
+    num:number
+}
+
+type cardName={
+    name:string
+}
+
+type cardDetails=cardNumber & cardName &{val:number}
+
+let k:cardDetails={
+    num:25,
+    name:"Abhi",
+    val:25000
+}
+console.log(k);
+
+
