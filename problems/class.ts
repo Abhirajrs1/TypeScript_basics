cbe7356a10f25f45dafc8186d82be846cefb1e26
Abhirajrs1 <132868953+Abhirajrs1@users.noreply.github.com>
2025-02-27 12:34:22 +0530

    Class with public and private

--- problems/class.ts
+++ problems/class.ts
@@ -2,7 +2,7 @@
 
 class Name{
     name:string
-    age:number
+    private age:number
     constructor(name:string,age:number){
     this.name=name
     this.age=age
@@ -11,3 +11,14 @@ class Name{
 let n=new Name("Abhiraj",26)
 n.name="Alan"
 console.log(n.name)
+
+// Modern and professional way to pass the values and access in the class
+
+class Gender{
+    constructor(public gender:string,private id:number){
+    }
+}
+let gend=new Gender("Male",1)
+gend.gender="Female"
+// gend.id=25 //We can't access it outside the class
+console.log(gend.gender)
